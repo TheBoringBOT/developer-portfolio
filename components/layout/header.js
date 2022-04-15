f939fa1069b6a76d4b584138af2b7a105439cea9
TheBoringBot <richardsgareth@icloud.com>
2022-04-15 13:57:14 +0200

    content change

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,7 +1,18 @@
 export default function Header() {
   return (
     <>
-      <div className="flex justify-between py-10 items-center"></div>
+      <div className="flex justify-between py-10 items-center sticky top-0 font-medium text-lg ">
+        <div className="flex items-start items-center">
+          <span className="rounded-full w-8 h-8 bg-black"></span>
+        </div>
+
+        <nav className="flex justify-end space-x-5 items-center">
+          <a href="#">Resume</a>
+          <a href="#" className="text-white bg-black px-5 py-1 rounded ">
+            Contact
+          </a>
+        </nav>
+      </div>
     </>
   );
 }
