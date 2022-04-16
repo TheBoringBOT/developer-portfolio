d390bcc0c797a53b3b65c11068f27ae97d0cdd91
TheBoringBot <richardsgareth@icloud.com>
2022-04-16 21:30:12 +0200

    removed header component

--- components/layout/index.js
+++ components/layout/index.js
@@ -4,13 +4,12 @@ import Footer from "./footer";
 export default function index({ children }) {
   return (
     <>
-      {/* two cols with left sticky starting larger screens */}
-      <div class="min-h-screen max-w-5xl mx-auto text-black">
-        <Header />
+      <div className="min-h-screen max-w-5xl mx-auto text-black">
+        {/* <Header /> */}
 
         <main>{children}</main>
-        <Footer />
       </div>
+      <Footer />
     </>
   );
 }
