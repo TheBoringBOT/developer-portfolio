7ce6a97e5ccbf2820aeb0f0a8f56fb6474de2310
TheBoringBot <richardsgareth@icloud.com>
2022-04-16 21:29:59 +0200

    new content

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,13 +1,28 @@
 import { getFullYear } from "../../utils/date-formatter";
-import Image from "next/image";
+import { IoChevronForward } from "react-icons/io5";
 import { constants } from "../../libs/constants";
 
 export default function footer() {
   const { site_author, site_version, site_description } = constants;
   return (
-    <footer className="mx-auto flex text-center justify-center">
-      <span>{getFullYear()} All rights reserved.</span>
-      <span>Version: {site_version}</span>
+    <footer className="h-[90vh] min-h-[600px] max-h-[900px] w-full p-0 m-0 mx-auto flex text-center justify-center bg-light-grey">
+      <div className="max-w-5xl mx-auto items-center flex flex-col px-10 md:px-0">
+        <div className="flex flex-col justify-center items-center mt-auto py-10">
+          <h4 className="gradient-title text-6xl md:text-8xl font-bold  ">
+            Would you like to contact me?
+          </h4>
+          <button className="text-blue font-medium text-2xl mt-8 hover:underline">
+            <span className="flex items-center space-x-4">
+              {" "}
+              Send me a message <IoChevronForward />{" "}
+            </span>
+          </button>
+        </div>
+        <div className="flex flex-col text-center justify-center  mt-auto py-10 text-dark-grey">
+          <span>{getFullYear()} All rights reserved.</span>
+          <span>Version: {site_version}</span>
+        </div>
+      </div>
     </footer>
   );
 }
