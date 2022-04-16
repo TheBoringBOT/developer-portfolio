a7772640fe78af1bba7a530113f0453e0637651e
TheBoringBot <richardsgareth@icloud.com>
2022-04-16 21:29:03 +0200

    new content

---
+++ components/hero/intro.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { IoChevronForward } from "react-icons/io5";
+import Avatar from "./avatar";
+
+export default function intro() {
+  return (
+    <>
+      <section className="h-[75vh] min-h-[650px]  flex flex-col  items-center text-center lg:max-w-5xl mx-auto">
+        <div className="my-auto flex flex-col  items-center ">
+          <Avatar />
+          <div>
+            <h1 className="text-[2.3rem] leading-4 font-bold text-grey block">
+              Full Stack Developer.
+            </h1>
+          </div>
+          <div>
+            <span className="text-4xl lg:text-[5.3rem] font-bold   mt-5 mx-20 leading-snug">
+              Gareth Richards
+            </span>
+          </div>
+          <div>
+            <button className="text-blue font-semibold text-3xl mt-4 hover:underline">
+              <span className="flex items-center space-x-4">
+                {" "}
+                Contact Me <IoChevronForward />{" "}
+              </span>
+            </button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
