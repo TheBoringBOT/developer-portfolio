8d35cdf3e52c69de4e80cbdc8301d7e0d8e24440
TheBoringBot <richardsgareth@icloud.com>
2022-04-19 16:47:17 +0200

    projects dummy data

---
+++ data/projects.js
@@ -0,0 +1,50 @@
+export const projectsData = {
+  1: {
+    title: "Project 1",
+    image:
+      "https://placehold.jp/30/dd6699/ffffff/600x600.png?text=placeholder+image+1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    description_short:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    stack: ["React", "Next.js", "Node.js", "Express"],
+    url: "https://www.google.com",
+    code: "https://www.google.com",
+  },
+  2: {
+    title: "Project 2",
+    image:
+      "https://placehold.jp/30/ffcc00/ffffff/600x600.png?text=placeholder+image+2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    description_short:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    stack: ["React", "Next.js", "Node.js", "Express"],
+    url: "https://www.google.com",
+    code: "https://www.google.com",
+  },
+  3: {
+    title: "Project 3",
+    image:
+      "https://placehold.jp/30/9873ff/ffffff/600x600.png?text=placeholder+image+3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    description_short:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    stack: ["React", "Next.js", "Node.js", "Express"],
+    url: "https://www.google.com",
+    code: "https://www.google.com",
+  },
+  4: {
+    title: "Project 4",
+    image:
+      "https://placehold.jp/30/ff763c/ffffff/600x600.png?text=placeholder+image+4",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    description_short:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    stack: ["React", "Next.js", "Node.js", "Express"],
+    url: "https://www.google.com",
+    code: "https://www.google.com",
+  },
+};
