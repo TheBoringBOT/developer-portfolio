4c244926b50acc903124894f20dd902da3674305
TheBoringBot <richardsgareth@icloud.com>
2022-04-19 16:48:22 +0200

    project page structure

--- pages/project/[id].js
+++ pages/project/[id].js
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import Head from "next/head";
 import { IoChevronForward, IoChevronBack } from "react-icons/io5";
 import Link from "next/link";
 import ErrorPage from "next/error";
@@ -35,87 +36,101 @@ const fadeInUp = {
 
 const Product = ({ props }) => {
   // return 404 if no project matching id
-  if (props.project === "undefined") {
+  if (!props.project) {
     return <ErrorPage statusCode={404} />;
   }
-  const { title, image, description, stack, url, code } = props.project;
+  const { title, image, description, description_short, stack, url, code } =
+    props.project;
 
   return (
-    <motion.div exit={{ opacity: 0 }}>
-      <motion.div initial="initial" animate="animate">
-        <div className="grid grid-cols-6 md:grid-cols-12 min-h-screen bg-light-grey">
-          {/* Project mockup */}
-          <div className="box col-span-6 lg:h-full">
-            <div className="flex justify-center items-center h-full">
-              <motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }}>
-                <motion.img
-                  className="h-full min-h-[300px] max-h-[45vh] p-10"
-                  key={image}
-                  src={image}
-                  animate={{ x: 0, opacity: 1 }}
-                  initial={{ x: 200, opacity: 0 }}
-                  exit={{ opacity: 0 }}
-                  transition={{ delay: 0.2 }}
-                />
-              </motion.div>
+    <>
+    {/* custom seo for page */}
+      <Head>
+        <title>
+          Project
+          {" | "}
+          {title}
+        </title>
+        <meta name="description" content={ description_short} />
+      </Head>
+      <motion.div exit={{ opacity: 0 }}>
+        <motion.div initial="initial" animate="animate">
+          <div className="grid grid-cols-6 md:grid-cols-12 min-h-screen bg-light-grey">
+            {/* Project mockup */}
+            <div className="box col-span-6 lg:h-full">
+              <div className="flex justify-center items-center h-full">
+                <motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }}>
+                  <motion.img
+                    className="h-full min-h-[300px] max-h-[45vh] p-10"
+                    key={image}
+                    src={image}
+                    animate={{ x: 0, opacity: 1 }}
+                    initial={{ x: 200, opacity: 0 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ delay: 0.2 }}
+                  />
+                </motion.div>
+              </div>
             </div>
-          </div>
 
-          {/* project details */}
-          <div className="box col-span-6 bg-white lg:h-full">
-            <div className="flex flex-col items-center lg:justify-start  px-10 py-16 md:py-40 lg:px-20  2xl:px-40 h-full">
-              <motion.div variants={stagger} className="space-y-10">
-                <Link href="/">
+            {/* project details */}
+            <div className="box col-span-6 bg-white lg:h-full">
+              <div className="flex flex-col items-center lg:justify-start  px-10 py-16 md:py-40 lg:px-20  2xl:px-40 h-full">
+                <motion.div variants={stagger} className="space-y-10">
+                  <Link href="/">
+                    <motion.div variants={fadeInUp}>
+                      <button className="text-blue hover:text-blue-hover  transition-all duration-200 hover:underline">
+                        <span className="flex items-center space-x-4 ">
+                          <IoChevronBack /> Back to home
+                        </span>
+                      </button>
+                    </motion.div>
+                  </Link>
                   <motion.div variants={fadeInUp}>
-                    <button className="text-blue hover:text-blue-hover  transition-all duration-200 hover:underline">
-                      <span className="flex items-center space-x-4 ">
-                        <IoChevronBack /> Back to home
-                      </span>
-                    </button>
+                    <span>Project</span>
                   </motion.div>
-                </Link>
-                <motion.div variants={fadeInUp}>
-                  <span>Project</span>
-                </motion.div>
-                <motion.h1
-                  className="text-4xl lg:text-6xl leading-tight font-bold"
-                  variants={fadeInUp}
-                >
-                  {title}
-                </motion.h1>
-                <motion.p variants={fadeInUp} className="text-xl font-normal">
-                  {description}
-                </motion.p>
-                <motion.div variants={fadeInUp} className="space-y-5">
-                  <span className="font-semibold">Tech Stack:</span>
-                  <div className="space-y-5 space-x-5">{techStack(stack)}</div>
-                </motion.div>
+                  <motion.h1
+                    className="text-4xl lg:text-6xl leading-tight font-bold"
+                    variants={fadeInUp}
+                  >
+                    {title}
+                  </motion.h1>
+                  <motion.p variants={fadeInUp} className="text-xl font-normal">
+                    {description}
+                  </motion.p>
+                  <motion.div variants={fadeInUp} className="space-y-5">
+                    <span className="font-semibold">Tech Stack:</span>
+                    <div className="space-y-5 space-x-5">
+                      {techStack(stack)}
+                    </div>
+                  </motion.div>
 
-                <motion.div variants={fadeInUp} className=" mt-20">
-                  <div className="space-x-10 flex items-center">
-                    <a
-                      href={url}
-                      className="bg-blue hover:bg-blue-hover transition-all duration-200 rounded px-5 py-2 text-white font-semibold"
-                    >
-                      View Live
-                    </a>
-                    <a
-                      href={code}
-                      className=" transition-all  text-dark-grey hover:text-black font-semibold  hover:underline"
-                    >
-                      <span className="flex items-center space-x-4">
-                        {" "}
-                        View Code <IoChevronForward />{" "}
-                      </span>
-                    </a>
-                  </div>
+                  <motion.div variants={fadeInUp} className=" mt-20">
+                    <div className="space-x-10 flex items-center">
+                      <a
+                        href={url}
+                        className="bg-blue hover:bg-blue-hover transition-all duration-200 rounded px-5 py-2 text-white font-semibold"
+                      >
+                        View Live
+                      </a>
+                      <a
+                        href={code}
+                        className=" transition-all  text-dark-grey hover:text-black font-semibold  hover:underline"
+                      >
+                        <span className="flex items-center space-x-4">
+                          {" "}
+                          View Code <IoChevronForward />{" "}
+                        </span>
+                      </a>
+                    </div>
+                  </motion.div>
                 </motion.div>
-              </motion.div>
+              </div>
             </div>
           </div>
-        </div>
+        </motion.div>
       </motion.div>
-    </motion.div>
+    </>
   );
 };
 
