c97cdbd4d9e7d2ee28312947680b1a359eeae7bc
TheBoringBot <richardsgareth@icloud.com>
2022-04-19 16:48:10 +0200

    modal functionality created

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
+import { useState } from "react";
 import Head from "next/head";
-import Image from "next/image";
+import { motion } from "framer-motion";
+
+//import modal
+import Modal from "../components/modal";
 
 //import layout
 import Layout from "../components/layout";
@@ -13,27 +17,34 @@ import { constants } from "../libs/constants";
 import useWindowSize from "../hooks/useWindowSize";
 
 export default function Home() {
+  const [modalOpen, setModalOpen] = useState(false);
   const { site_name, site_author, site_description } = constants;
 
   // check if device is mobile - used mostly to stop some animations on mobile
   const isMobile = useWindowSize().width < 768;
   return (
-    <div>
-      <Head>
-        <title>
-          {site_author}
-          {" | "}
-          {site_name}
-        </title>
-        <meta name="description" content={site_description} />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <motion.div exit={{ opacity: 0 }}>
+        <Head>
+          <title>
+            {site_author}
+            {" | "}
+            {site_name}
+          </title>
+          <meta name="description" content={site_description} />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <Layout>
-        <Intro />
-        <Projects runAnimation={isMobile ? false : true} />
-        <About />
-      </Layout>
-    </div>
+        <Layout setModalOpen={setModalOpen}>
+          {modalOpen && <Modal setModalOpen={setModalOpen} />}
+          <a onClick={() => setModalOpen(true)} className="text-6xl text-blue">
+            <h1>open</h1>
+          </a>
+          <Intro setModalOpen={setModalOpen} />
+          <Projects runAnimation={isMobile ? false : true} />
+          <About />
+        </Layout>
+      </motion.div>
+    </>
   );
 }
