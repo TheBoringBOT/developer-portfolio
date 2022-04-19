108f070e23d1173471b36fc88a6559b66114dada
TheBoringBot <richardsgareth@icloud.com>
2022-04-19 10:41:43 +0200

    new content added

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,17 @@ import Layout from "../components/layout";
 
 // import components
 import Intro from "../components/hero/intro";
-import Projects from "../components/projects";
+import Projects from "../components/projects/projects";
+import About from "../components/about/about";
 
 import { constants } from "../libs/constants";
+import useWindowSize from "../hooks/useWindowSize";
 
 export default function Home() {
   const { site_name, site_author, site_description } = constants;
+
+  // check if device is mobile - used mostly to stop some animations on mobile
+  const isMobile = useWindowSize().width < 768;
   return (
     <div>
       <Head>
@@ -26,7 +31,8 @@ export default function Home() {
 
       <Layout>
         <Intro />
-        <Projects />
+        <Projects runAnimation={isMobile ? false : true} />
+        <About />
       </Layout>
     </div>
   );
