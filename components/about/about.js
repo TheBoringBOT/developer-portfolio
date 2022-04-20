b8285c223a7a2caeb1972d501f4e020650bcf174
TheBoringBot <richardsgareth@icloud.com>
2022-04-20 22:24:59 +0200

    cleaned up content

--- components/about/about.js
+++ components/about/about.js
@@ -1,18 +1,19 @@
-import React from "react";
+import { useRef, useState, useEffect } from "react";
 import { IoChevronForward } from "react-icons/io5";
-import { motion } from "framer-motion";
+
+import { motion, useViewportScroll } from "framer-motion";
 
 export default function about() {
   return (
     <>
-      <div className="space-y-20 my-44 text-4xl   lg:text-[5.3rem] leading-tight  font-bold">
-        {aboutText.map((text, index) => {
-          return (
-            <div key={index}>
-              <motion.p>{text}</motion.p>
-            </div>
-          );
-        })}
+      <div className="h-full space-y-20 my-44 text-4xl   lg:text-[5.3rem] leading-tight  font-bold">
+        <p>I’m a full stack developer with a diverse skill set. </p>
+
+        <p>In areas such as design, code, marketing & business.</p>
+        <p>I build with React, Laravel, Gatsby, Mysql and more...</p>
+        <p>I have worked with clients all over the 🌎 </p>
+        <p>I am 🇬🇧 but based in Sweden. </p>
+        <p>Want to know more? </p>
         <button className="text-highlight  font-bold  mt-8 hover:underline hover:text-highlight-hover  transition-all duration-200">
           <span className="flex items-center space-x-4">
             {" "}
@@ -23,14 +24,3 @@ export default function about() {
     </>
   );
 }
-
-//about text split into array for each paragraph
-const aboutText = [
-  "I’m a full stack developer with a diverse skill set.",
-  "In areas such as design, code, marketing & business.",
-  "I build with React, Laravel, Gatsby, Mysql and more... ",
-  "I have worked with clients all over the 🌎 ",
-  "I am 🇬🇧 but based in Sweden.",
-  "Want to know more?",
-  ,
-];
