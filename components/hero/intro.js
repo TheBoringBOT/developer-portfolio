e68df4b3a2139272257ffe6c3b24946d4d471310
TheBoringBot <richardsgareth@icloud.com>
2022-04-22 00:12:03 +0200

    style: tighter tracking on title

--- components/hero/intro.js
+++ components/hero/intro.js
@@ -34,22 +34,22 @@ const fadeInUp = {
 export default function intro() {
   return (
     <>
-      <section className="h-[75vh] min-h-[650px]  flex flex-col  items-center text-center lg:max-w-5xl mx-auto">
+      <section className="h-[75vh] min-h-[650px]  flex flex-col  items-center text-center lg:max-w-5xl   mx-auto">
         <div className="my-auto flex flex-col  items-center ">
           <Avatar />
           <motion.div variants={stagger} initial="initial" animate="animate">
             <motion.div variants={fadeInUp}>
-              <h1 className=" text-3xl leading-tight md:text-[2.3rem]  font-bold text-grey block">
+              <h1 className=" tracking-tight text-3xl leading-tight md:text-[2.3rem]  font-bold text-grey block">
                 Full Stack Developer.
               </h1>
             </motion.div>
             <motion.div variants={fadeInUp}>
-              <span className="text-[3.3rem] leading-tight md:text-6xl lg:text-[5.3rem] font-bold  ">
+              <span className=" tracking-tight text-[3.3rem] leading-tight md:text-6xl lg:text-[5.3rem] font-bold  ">
                 Gareth Richards
               </span>
             </motion.div>
             <motion.div variants={fadeInUp}>
-              <button className="text-highlight font-semibold text-3xl mt-4 hover:underline hover:text-highlight-hover">
+              <button className="tracking-tight text-highlight font-semibold text-3xl mt-4 hover:underline hover:text-highlight-hover">
                 <span className="flex items-center space-x-4">
                   {" "}
                   Contact Me <IoChevronForward />{" "}
