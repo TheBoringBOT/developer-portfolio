ef31e070508c246f353c83e5b026a943841f23c0
TheBoringBot <richardsgareth@icloud.com>
2022-04-24 19:24:27 +0200

    style: polished and simplified look

--- components/contact-form.js
+++ components/contact-form.js
@@ -44,54 +44,54 @@ export default function contactForm({ props, setModalOpen }) {
       onSubmit={handleSubmit(submitForm)}
     >
       <div className="flex flex-wrap -mx-3 mb-6">
-        <div className="w-full px-3 mb-6 md:mb-0">
-          <label className="block tracking-wide text-black text-xs font-semibold mb-2">
+        <div className="w-full px-3 my-6 md:mb-0">
+          {/* <label className="block tracking-wide text-black text-xs font-semibold mb-2">
             Name
-          </label>
+          </label> */}
           <input
             {...register("name", { required: true, maxLength: 20 })}
-            className="appearance-none block w-full bg-light-grey text-black border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            className="appearance-none block w-full bg-light-grey text-black border  border-light-grey rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
             type="text"
             required
             name="name"
-            placeholder="Steve Jobs"
+            placeholder="Name"
           />
         </div>
       </div>
       <div className="flex flex-wrap -mx-3 mb-6">
         <div className="w-full px-3">
-          <label className="block tracking-wide text-black text-xs font-semibold mb-2">
+          {/* <label className="block tracking-wide text-black text-xs font-semibold mb-2">
             E-mail
-          </label>
+          </label> */}
           <input
             {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
-            className="appearance-none block w-full bg-light-grey text-black border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            className="appearance-none block w-full bg-light-grey text-black border border-light-grey rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             type="email"
             name="email"
             required
-            placeholder="steve@apple.com"
+            placeholder="Email"
           />
         </div>
       </div>
       <div className="flex flex-wrap -mx-3 mb-6">
         <div className="w-full px-3">
-          <label className="block  tracking-wide text-black text-xs font-semibold mb-2">
+          {/* <label className="block  tracking-wide text-black text-xs font-semibold mb-2">
             Message
-          </label>
+          </label> */}
           <textarea
             {...register("message", { required: true, maxLength: 300 })}
-            className=" no-resize appearance-none block w-full bg-light-grey text-black border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+            className=" no-resize appearance-none block w-full bg-light-grey text-black border border-light-grey  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
             required
             name="message"
-            maxlength="300"
-            placeholder="I want to carry my music collection in my pocket, let's talk about it..."
+            maxLength="300"
+            placeholder="Message."
           ></textarea>
           <div className="flex items-center justify-start">
             <p className="text-gray-600 text-xs italic mr-2">
               Don't like forms?
             </p>
             <a
-              className=" text-xs underline text-highlight hover:text-highlight-hover"
+              className=" text-xs underline text-gray-600"
               href="mailto:hi@garethrichards.dev"
             >
               hi@garethrichards.dev
