b88ea54f4c2de6de8632125c995aa2c72de357de
TheBoringBot <richardsgareth@icloud.com>
2022-05-27 12:43:12 +0200

    style: small changes

--- components/contact-form.js
+++ components/contact-form.js
@@ -43,14 +43,14 @@ export default function contactForm({ props, setModalOpen }) {
       ref={form}
       onSubmit={handleSubmit(submitForm)}
     >
-      <div className="flex flex-wrap -mx-3 mb-6">
+      <div className="flex flex-wrap mb-6 -mx-3">
         <div className="w-full px-3 my-6 md:mb-0">
-          {/* <label className="block tracking-wide text-black text-xs font-semibold mb-2">
+          {/* <label className="block mb-2 text-xs font-semibold tracking-wide text-black">
             Name
           </label> */}
           <input
             {...register("name", { required: true, maxLength: 20 })}
-            className="appearance-none block w-full bg-light-grey text-black border  border-light-grey rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            className="block w-full px-4 py-3 mb-3 leading-tight text-black border rounded appearance-none bg-light-grey border-light-grey focus:outline-none focus:bg-white"
             type="text"
             required
             name="name"
@@ -58,14 +58,14 @@ export default function contactForm({ props, setModalOpen }) {
           />
         </div>
       </div>
-      <div className="flex flex-wrap -mx-3 mb-6">
+      <div className="flex flex-wrap mb-6 -mx-3">
         <div className="w-full px-3">
-          {/* <label className="block tracking-wide text-black text-xs font-semibold mb-2">
+          {/* <label className="block mb-2 text-xs font-semibold tracking-wide text-black">
             E-mail
           </label> */}
           <input
             {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
-            className="appearance-none block w-full bg-light-grey text-black border border-light-grey rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            className="block w-full px-4 py-3 mb-3 leading-tight text-black border rounded appearance-none bg-light-grey border-light-grey focus:outline-none focus:bg-white focus:border-gray-500"
             type="email"
             name="email"
             required
@@ -73,25 +73,25 @@ export default function contactForm({ props, setModalOpen }) {
           />
         </div>
       </div>
-      <div className="flex flex-wrap -mx-3 mb-6">
+      <div className="flex flex-wrap mb-6 -mx-3">
         <div className="w-full px-3">
-          {/* <label className="block  tracking-wide text-black text-xs font-semibold mb-2">
+          {/* <label className="block mb-2 text-xs font-semibold tracking-wide text-black">
             Message
           </label> */}
           <textarea
             {...register("message", { required: true, maxLength: 300 })}
-            className=" no-resize appearance-none block w-full bg-light-grey text-black border border-light-grey  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+            className="block w-full h-48 px-4 py-3 mb-3 leading-tight text-black border rounded appearance-none resize-none no-resize bg-light-grey border-light-grey focus:outline-none focus:bg-white focus:border-gray-500"
             required
             name="message"
             maxLength="300"
             placeholder="Message."
           ></textarea>
           <div className="flex items-center justify-start">
-            <p className="text-gray-600 text-xs italic mr-2">
+            <p className="mr-2 text-xs italic text-gray-600">
               Don't like forms?
             </p>
             <a
-              className=" text-xs underline text-gray-600"
+              className="text-xs text-gray-600 underline "
               href="mailto:hi@garethrichards.dev"
             >
               hi@garethrichards.dev
@@ -102,7 +102,7 @@ export default function contactForm({ props, setModalOpen }) {
       <div className="md:flex md:items-center">
         <div className="md:w-1/3">
           <button
-            className="shadow whitespace-nowrap bg-highlight hover:bg-highlight-hover transition-all focus:shadow-outline focus:outline-none text-white font-semibold py-2 px-4 rounded"
+            className="px-4 py-2 font-semibold text-white transition-all rounded shadow whitespace-nowrap bg-highlight hover:bg-highlight-hover focus:shadow-outline focus:outline-none"
             type="submit"
           >
             <span>{buttonText}</span>
