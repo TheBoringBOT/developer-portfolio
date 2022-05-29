dbc387715c024688f8dca7448f5c81d20188c334
TheBoringBot <richardsgareth@icloud.com>
2022-05-29 18:05:15 +0200

    feat: default theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import { AnimatePresence } from "framer-motion";
+import { ThemeProvider } from "next-themes";
+
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AnimatePresence exitBeforeEnter>
-      <Component {...pageProps} />
-    </AnimatePresence>
+    <ThemeProvider attribute="class" defaultTheme="dark">
+      <AnimatePresence exitBeforeEnter>
+        <Component {...pageProps} />
+      </AnimatePresence>
+    </ThemeProvider>
   );
 }
 
