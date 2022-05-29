e53d313f17724d453ca79dab6c7bf19b8ff2ad81
TheBoringBot <richardsgareth@icloud.com>
2022-05-29 18:04:47 +0200

    style: animation smoothing when theme changed

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -5,7 +5,8 @@ export default function Layout({ children, setModalOpen }) {
   return (
     <>
       {/* <Header /> */}
-      <main className="min-h-screen mx-auto text-black min-w-screen">
+
+      <main className="min-h-screen mx-auto text-black duration-300 bg-white  dark:text-light-grey dark:bg-black min-w-screen transition-bg">
         {children}
       </main>
       <Footer setModalOpen={setModalOpen} />
