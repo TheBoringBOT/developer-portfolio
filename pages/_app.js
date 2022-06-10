2beab990221d169bf70f9eae33377064c7478891
TheBoringBot <richardsgareth@icloud.com>
2022-06-10 19:33:03 +0200

    feat: google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,52 @@
+import { useEffect } from "react";
 import { AnimatePresence } from "framer-motion";
 import { ThemeProvider } from "next-themes";
+import { useRouter } from "next/router";
+import { GA_TRACKING_ID } from "../libs/gTag";
+import Script from "next/script";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark">
-      <AnimatePresence exitBeforeEnter>
-        <Component {...pageProps} />
-      </AnimatePresence>
-    </ThemeProvider>
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        <AnimatePresence exitBeforeEnter>
+          <Component {...pageProps} />
+        </AnimatePresence>
+      </ThemeProvider>
+    </>
   );
 }
 
